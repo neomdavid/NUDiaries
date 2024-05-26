27beee698002a4d8a1b6c5aee27f17e0db982316
neomdavid <neomanaogdavid@gmail.com>
2024-05-26 22:21:25 +0800

    Apply floating container

---
+++ styles/floating.js
@@ -0,0 +1,14 @@
+
+const wrapper = document.querySelector('.wrapper');
+const floating = document.querySelector('.write-message-container');
+
+document.querySelector('.js-write-button').addEventListener('click',()=>{
+  wrapper.classList.add('visible');
+  floating.classList.add('visible');
+});
+
+document.querySelector('.exit-button').addEventListener('click',()=>{
+  console.log(wrapper.classList);
+  wrapper.classList.remove('visible');
+  floating.classList.remove('visible');
+});
