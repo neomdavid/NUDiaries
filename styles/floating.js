48f4eb66c83748b40c312525ce4dfb4a866c71fc
neomdavid <neomanaogdavid@gmail.com>
2024-05-27 22:11:51 +0800

    Fixed the classlist visible, put it at the bottom so that it wont be overwritten

--- styles/floating.js
+++ styles/floating.js
@@ -1,6 +1,7 @@
 
 const wrapper = document.querySelector('.wrapper');
 const floating = document.querySelector('.write-message-container');
+console.log(floating);
 
 document.querySelector('.js-write-button').addEventListener('click',()=>{
   wrapper.classList.add('visible');
@@ -11,4 +12,5 @@ document.querySelector('.exit-button').addEventListener('click',()=>{
   console.log(wrapper.classList);
   wrapper.classList.remove('visible');
   floating.classList.remove('visible');
+  
 });
