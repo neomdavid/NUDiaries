ea4223f75ca4b8bb6d06c7a144f5afe90d0a2823
neomdavid <neomanaogdavid@gmail.com>
2024-06-01 10:42:29 +0800

    Fix bug: adding a post not only once; problem: did not pass event as a parameter

--- scripts/data/posts.js
+++ scripts/data/posts.js
@@ -1,14 +1,25 @@
 export let posts = JSON.parse(localStorage.getItem('posts')) || 
 [{
-  postId:'1',
-  author:'Neo',
-  title:'Ang title nito ay',
-  message:'anofwaefawefawefsawnfoaefkanwefknaskdnflkanweklfnklasdnflkanwklefnklandsklfnaklwdnfklanwdklfnawklndfkawawefkwemfkanwekfnkawfwafewaawfeawakefnkajwnefawknefknawekfnkawenfkawnfwakefmkwafnkwaefnkawnefkaweekewnwaelfawlmefwlmeflawmflemwflaemflawwaefawefawfeawes',//255 characters
-  theme:'rgb(99, 211, 130)',
-  topic:'images/technology.png'
+  postId: '1',
+  author: 'Neo',
+  title: 'Ang title nito ay',
+  message: 'anofwaefawefawefsawnfoaefkanwefknaskdnflkanweklfnklasdnflkanwklefnklandsklfnaklwdnfklanwdklfnawklndfkawawefkwemfkanwekfnkawfwafewaawfeawakefnkajwnefawknefknawekfnkawenfkawnfwakefmkwafnkwaefnkawnefkaweekewnwaelfawlmefwlmeflawmflemwflaemflawwaefawefawfeawes', //255 characters
+  theme: 'rgb(99, 211, 130)',
+  topic: 'images/technology.png'
 }];
 
-export function saveToStorage(){
+export function saveToStorage() {
   localStorage.setItem('posts', JSON.stringify(posts));
 }
 
+export function addPost(postId, author, title, message, theme, topic) {
+  posts.push({
+    postId,
+    author,
+    title,
+    message,
+    theme,
+    topic
+  });
+  saveToStorage();
+}
--- scripts/wall.js
+++ scripts/wall.js
@@ -1,89 +1,77 @@
-import { posts, saveToStorage} from "./data/posts.js";
+import { posts, addPost } from "./data/posts.js";
 
-renderWall();
-function renderWall(){
+function renderWall() {
   let postsHTML = ``;
-posts.forEach((post)=>{
-  postsHTML+=`
-  <div class="post-container">
-  <div class="profile-container">
-    <div class="profile-image-container">
-      <div></div>
-    </div>
-    <div class="details-container">
-      <p class="title">${post.title}</p>
-      <p class="name">${post.author}</p>
-      <p class="status">Active 2 days ago</p>
-    </div>
-    <div class="topic-image-container">
-      <img src="${post.topic}">
-    </div>
-  </div>
-  <div class="message-container">
-    <p style="background-color:${post.theme}">${post.message}</p>  
-  </div>
-</div> 
-`
-});
+  posts.forEach((post) => {
+    postsHTML += `
+      <div class="post-container">
+        <div class="profile-container">
+          <div class="profile-image-container">
+            <div></div>
+          </div>
+          <div class="details-container">
+            <p class="title">${post.title}</p>
+            <p class="name">${post.author}</p>
+            <p class="status">Active 2 days ago</p>
+          </div>
+          <div class="topic-image-container">
+            <img src="${post.topic}">
+          </div>
+        </div>
+        <div class="message-container">
+          <p style="background-color:${post.theme}">${post.message}</p>  
+        </div>
+      </div>
+    `;
+  });
 
-document.querySelector('.js-posts-list-container').innerHTML = postsHTML;
+  document.querySelector('.js-posts-list-container').innerHTML = postsHTML;
+}
 
 let theme;
 let topic;
-document.querySelector('.js-post-button').addEventListener('click',()=>{
+
+document.querySelector('.js-post-button').addEventListener('click', () => {
   const author = document.querySelector('.author-container input').value;
   const title = document.querySelector('.title-container input').value;
   const message = document.querySelector('.messager-container textarea').value;
-  posts.push({
-    postId: posts.length,
-    author,
-    title,
-    message,
-    theme,
-    topic
-  })
-  saveToStorage();
+  
+  addPost(posts.length + 1, author, title, message, theme, topic);
+
   wrapper.classList.remove('visible');
   floating.classList.remove('visible');
   renderWall();
 });
 
-//THEME
-document.querySelector('.js-blue').addEventListener('click',()=>{
-  console.log('blue');
+// THEME
+document.querySelector('.js-blue').addEventListener('click', () => {
   theme = 'rgb(99, 177, 211)';
 });
-document.querySelector('.js-red').addEventListener('click',()=>{
-  console.log('red');
+document.querySelector('.js-red').addEventListener('click', () => {
   theme = 'rgb(211, 99, 99)';
 });
-document.querySelector('.js-green').addEventListener('click',()=>{
-  console.log('green');
+document.querySelector('.js-green').addEventListener('click', () => {
   theme = 'rgb(99, 211, 130)';
 });
-document.querySelector('.js-yellow').addEventListener('click',()=>{
-  console.log('yellow');
+document.querySelector('.js-yellow').addEventListener('click', () => {
   theme = 'rgb(233, 223, 129)';
 });
 
-//TOPIC
-document.querySelector('.js-technology').addEventListener('click',()=>{
-  const src = event.target.getAttribute('src');
-  topic = src;
+// TOPIC
+document.querySelector('.js-technology').addEventListener('click', (event) => {
+  topic = event.target.getAttribute('src');
 });
-document.querySelector('.js-literature').addEventListener('click',()=>{
-  const src = event.target.getAttribute('src');
-  topic = src;
+document.querySelector('.js-literature').addEventListener('click', (event) => {
+  topic = event.target.getAttribute('src');
 });
-document.querySelector('.js-art').addEventListener('click',()=>{
-  const src = event.target.getAttribute('src');
-  topic = src;
+document.querySelector('.js-art').addEventListener('click', (event) => {
+  topic = event.target.getAttribute('src');
 });
-document.querySelector('.js-politics').addEventListener('click',()=>{
-  const src = event.target.getAttribute('src');
-  topic = src;
+document.querySelector('.js-politics').addEventListener('click', (event) => {
+  topic = event.target.getAttribute('src');
 });
 
 const wrapper = document.querySelector('.wrapper');
 const floating = document.querySelector('.write-message-container');
-}
+
+renderWall();
